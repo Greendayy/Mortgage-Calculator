53ac3d1627b147c1fd49d797a29d360b69e902c6
greenday <974023720@qq.com>
2023-02-24 23:19:02 +0800

    add form

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
@@ -14,15 +12,119 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <h1 className="text-3xl font-bold">计算方式</h1>
-        </div>
-
-        <div className={styles.description}>
-          <h1 className="text-3xl font-bold">贷款方式</h1>
+        <nav className={styles.tab}>
+          <h1 className={styles.tabTittle}>计算方式</h1>
+          <div className={styles.tabButton}>
+            <button className={styles.tabButton1}>按贷款总额</button>
+            <button className={styles.tabButton2}>按房屋总价</button>
+          </div>
+        </nav>
+        <form action="" method="get">
+          <div className={styles.form}>
+            <label className={styles.label} htmlFor="name">
+              贷款金额{" "}
+            </label>
+            <input
+              className={styles.input}
+              type="num"
+              name="dkje"
+              id="dkje"
+              defaultValue={0}
+              required
+            />
+            <span className={styles.unit}>万</span>
+          </div>
+        </form>
+        <hr className={styles.hr} />
+        <nav className={styles.tab}>
+          <h1 className={styles.tabTittle}>计算方式</h1>
+          <div className={styles.tabButton}>
+            <button className={styles.tabButton3}>组合贷</button>
+            <button className={styles.tabButton4}>商业贷</button>
+            <button className={styles.tabButton5}>公积金贷</button>
+          </div>
+        </nav>
+        <div className={styles.formMain}>
+          <form action="" method="get">
+            <div className={styles.form}>
+              <label className={styles.label} htmlFor="name">
+                商贷年限{" "}
+              </label>
+              <input
+                className={styles.input}
+                type="num"
+                name="sdnx"
+                id="sdnx"
+                defaultValue="30年"
+                placeholder="30年"
+              />
+              <span className={styles.unit}>></span>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.form}>
+              <label className={styles.label} htmlFor="name">
+                利率方式{" "}
+              </label>
+              <input
+                className={styles.input}
+                type="num"
+                name="llfs"
+                id="llfs"
+                placeholder="使用最新LPR"
+                defaultValue="使用最新LPR"
+              />
+              <span className={styles.unit}>></span>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.form}>
+              <label className={styles.label} htmlFor="name">
+                L P R{" "}
+              </label>
+              <input
+                className={styles.input}
+                type="num"
+                name="lpr"
+                id="lpr"
+                placeholder="4.65"
+                defaultValue={4.65}
+              />
+              <span className={styles.unit}>%</span>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.form}>
+              <label className={styles.label} htmlFor="name">
+                基 点{" "}
+              </label>
+              <input
+                className={styles.input}
+                type="num"
+                name="jd"
+                id="jd"
+                placeholder="0"
+                defaultValue={0}
+              />
+              <span className={styles.unit}>BP(‱)</span>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.form}>
+              <label className={styles.label} htmlFor="name">
+                商贷利率{" "}
+              </label>
+              <input
+                className={styles.input}
+                type="num"
+                name="sdll"
+                id="sdll"
+                placeholder="4.65%+0‱="
+                defaultValue=""
+              />
+              <span>4.65%</span>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.form}></div>
+          </form>
         </div>
       </main>
-
       <button className={styles.calcButton}>开始计算</button>
     </>
   );

c2c9d2377f800f62e93398015115158b9752c4ad
greenday <974023720@qq.com>
2023-03-05 11:53:29 +0800

    feat:add history table

--- pages/detail.jsx
+++ pages/detail.jsx
@@ -1,16 +1,35 @@
 import { Tab } from "antd-mobile/es/components/tabs/tabs";
 import Link from "next/link";
-import styles from "../styles/Tabel.module.css";
+import styles from "../styles/Table.module.css";
 
 export default function Detail() {
   return (
     <>
+      <head>
+        <title>房贷计算器-还款详情</title>
+      </head>
       <nav className={styles.nav}>
         <p>
           <Link href="/">返回</Link>
         </p>
         <h1>还款详情</h1>
       </nav>
+      <main className={styles.table}>
+        <section className={styles.section}>
+          <div className={styles.cell}>
+            <h2 className={styles.th}>公积金贷30年</h2>
+            <text className={styles.td}>10万</text>
+          </div>
+          <div className={styles.cell}>
+            <h2 className={styles.th}>商业贷30年</h2>
+            <text className={styles.td}>0万</text>
+          </div>
+          <div className={styles.cell}>
+            <h2 className={styles.th}>每月应还(等额本息)</h2>
+            <text className={styles.td}>435元</text>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
--- pages/history.tsx
+++ pages/history.tsx
@@ -1,15 +1,51 @@
 import Link from "next/link";
-import styles from "../styles/Tabel.module.css";
+import styles from "../styles/Table.module.css";
+import { Head } from "next/document";
 
 export default function History() {
   return (
     <>
+      <head>
+        <title>房贷计算器-历史记录</title>
+      </head>
       <nav className={styles.nav}>
         <p>
           <Link href="/">返回</Link>
         </p>
         <h1>历史记录</h1>
       </nav>
+      <main className={styles.table}>
+        <section className={styles.row}>
+          <div className={styles.cell}>
+            <h2 className={styles.th}>公积金贷30年</h2>
+            <text className={styles.td}>10万</text>
+          </div>
+          <div className={styles.cell}>
+            <h2 className={styles.th}>商业贷30年</h2>
+            <text className={styles.td}>0万</text>
+          </div>
+          <div className={styles.cell}>
+            <h2 className={styles.th}>每月应还(等额本息)</h2>
+            <text className={styles.td}>435元</text>
+          </div>
+        </section>
+        <hr className={styles.hr} />
+        <section className={styles.row}>
+          <div className={styles.cell}>
+            <h2 className={styles.th}>公积金贷30年</h2>
+            <text className={styles.td}>10万</text>
+          </div>
+          <div className={styles.cell}>
+            <h2 className={styles.th}>商业贷30年</h2>
+            <text className={styles.td}>0万</text>
+          </div>
+          <div className={styles.cell}>
+            <h2 className={styles.th}>每月应还(等额本息)</h2>
+            <text className={styles.td}>435元</text>
+          </div>
+        </section>
+        <hr className={styles.hr} />
+      </main>
     </>
   );
 }

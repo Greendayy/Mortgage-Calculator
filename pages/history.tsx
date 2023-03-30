4b8a277fc16827cd0f89ef15ed575721549c351a
greenday <974023720@qq.com>
2023-03-30 16:54:44 +0800

    init

--- pages/history.tsx
+++ pages/history.tsx
@@ -3,49 +3,5 @@ import styles from "../styles/History.module.css";
 import Head from "next/head";
 
 export default function History() {
-  return (
-    <>
-      <Head>
-        <title>房贷计算器-历史记录</title>
-      </Head>
-      <nav className={styles.nav}>
-        <p>
-          <Link href="/">{"ᐸ"}</Link>
-        </p>
-        <h1>历史记录</h1>
-      </nav>
-      <main className={styles.table}>
-        <section className={styles.row}>
-          <div className={styles.cell}>
-            <h2 className={styles.th}>公积金贷30年</h2>
-            <text className={styles.td}>10万</text>
-          </div>
-          <div className={styles.cell}>
-            <h2 className={styles.th}>商业贷30年</h2>
-            <text className={styles.td}>0万</text>
-          </div>
-          <div className={styles.cell}>
-            <h2 className={styles.th}>每月应还(等额本息)</h2>
-            <text className={styles.td}>435元</text>
-          </div>
-        </section>
-        <hr className={styles.hr} />
-        <section className={styles.row}>
-          <div className={styles.cell}>
-            <h2 className={styles.th}>公积金贷30年</h2>
-            <text className={styles.td}>10万</text>
-          </div>
-          <div className={styles.cell}>
-            <h2 className={styles.th}>商业贷30年</h2>
-            <text className={styles.td}>0万</text>
-          </div>
-          <div className={styles.cell}>
-            <h2 className={styles.th}>每月应还(等额本息)</h2>
-            <text className={styles.td}>435元</text>
-          </div>
-        </section>
-        <hr className={styles.hr} />
-      </main>
-    </>
-  );
+  return <>历史记录</>;
 }
